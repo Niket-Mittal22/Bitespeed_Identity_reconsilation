f3dee01eb4cc808854d6e13f94951f01390cc248
Niket Mittal <tanishmittal2004@gmail.com>
2026-02-25 23:24:17 +0530

    updated app.py for deployment

--- app.py
+++ app.py
@@ -27,4 +27,6 @@ def identify():
     return jsonify(result), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
